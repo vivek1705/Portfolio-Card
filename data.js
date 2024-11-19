86ffbe60e8efb0763e88a6d06fd43731bd6143e6
Vivek Patil <vivekvpatil1705@gmail.com>
2024-11-19 18:47:10 +0530

    Updated files for portfolio

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ const profileData = {
   profilePic: "images/mypic.jpeg",
   coverPic: "images/bgimg.png",
   description: "A highly skilled Software developer with a passion for crafting seamless user experiences. Proficient in JavaScript, React, and modern web technologies. Eager to bring ideas to life through thoughtful design and responsive interfaces.",
-  profileLink: "https://vivekpatil-portfolio.netlify.app/",
+  profileLink: "https://vivekpatil-portfolio.onrender.com/",
   cvLink: "https://drive.google.com/file/d/1UeGSiyXz1xOIhpQE94sl1KFLFhLVnMBe/view?usp=drive_link", // Replace with your Google Drive CV link
   location: "Pune, 411062",
   mobile: "+91 9763189395",
